a390d5c78fd3d65decbfa9fa256dcd41444d51f8
Faith Roeseler <faith.roeseler@gmail.com>
2017-12-12 20:34:26 -0600

    Almost done.  Have to figure out how to search for added name...

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.*;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -25,6 +26,9 @@ public class JobController {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
 
+        Job aJob = jobData.findById(id);
+        model.addAttribute("aJob", aJob);
+
         return "job-detail";
     }
 
@@ -41,7 +45,34 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        if (errors.hasErrors()) {
+            return "new-job";
 
+        }
+
+        String newName = jobForm.getName();
+
+        int employerId = jobForm.getEmployerId();
+        Employer newEmployer = jobData.getEmployers().findById(employerId);
+
+        int locationId = jobForm.getLocationId();
+        Location newLocation = jobData.getLocations().findById(locationId);
+
+        int coreCompetencyId = jobForm.getCoreCompetencyId();
+        CoreCompetency newCoreCompetency = jobData.getCoreCompetencies().findById(coreCompetencyId);
+
+        int positionTypeId = jobForm.getPositionTypeId();
+        PositionType newPositionType = jobData.getPositionTypes().findById(positionTypeId);
+
+        Job newJob = new Job(newName, newEmployer, newLocation, newPositionType, newCoreCompetency);
+
+        jobData.add(newJob);
+
+        // jerk move, guys
+
+        int newJobId = newJob.getId();
+
+        return "redirect:?id=" + newJobId;
     }
 }
+
